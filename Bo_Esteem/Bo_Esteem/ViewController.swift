0e3e6ad1e7a6d6671dd8ec371995e8e12fbd00af
lavaspoon <lavaspoon@naver.com>
2019-10-10 02:45:51 +0900

    //프로젝트 생성

---
+++ Bo_Esteem/Bo_Esteem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bo_Esteem
+//
+//  Created by lavaspoon on 10/10/2019.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bo_Esteem/Bo_EsteemTests/Bo_EsteemTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bo_EsteemTests.swift
+//  Bo_EsteemTests
+//
+//  Created by lavaspoon on 10/10/2019.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+
+import XCTest
+@testable import Bo_Esteem
+
+class Bo_EsteemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
